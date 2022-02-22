3f17007b454da87bdc13cc81cf6c3c13ce3ac2a0
Matt Cowan <matt@mattbcowan.com>
2022-02-22 17:13:07 -0600

    feat: navbar added

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,31 +1,60 @@
 import React, { useState } from "react";
+import { Sling as Hamburger } from "hamburger-react";
+import { motion } from "framer-motion";
 import "./Navbar.css";
+
+const variants = {
+  visible: {
+    opacity: 1,
+    x: "0",
+    ease: "easeIn",
+    duration: 0.3,
+    transition: {
+      when: "beforeChildren",
+      staggerChildren: 0.4,
+    },
+  },
+  hidden: {
+    opacity: 0,
+    x: "200%",
+    ease: "easeIn",
+    duration: 0.4,
+    transition: {
+      when: "afterChildren",
+    },
+  },
+};
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
     <nav className="navbar">
-      <div className="navbar--logo-container">
-        <img
-          className="navbar--logo"
-          src="/assets/desktop/logo.svg"
-          alt="Create Logo"
-        />
+      <div className="navbar--container">
+        <div className="navbar--logo-container">
+          <img
+            className="navbar--logo"
+            src="/assets/desktop/logo.svg"
+            alt="Create Logo"
+          />
+        </div>
+        <div className="navbar--hamburger-container">
+          <Hamburger color="#979797" toggled={isOpen} toggle={setIsOpen} />
+        </div>
       </div>
-      <div className="navbar--hamburger-container">
-        <button onClick={toggleMenu} type="button">
-          {isOpen ? (
-            <img src="/assets/mobile/icon-cross.svg" alt="hamburger icon" />
-          ) : (
-            <img src="/assets/mobile/icon-hamburger.svg" alt="hamburger icon" />
-          )}
-        </button>
-      </div>
+      <motion.div
+        className="navbar--dropdown"
+        initial="hidden"
+        animate={isOpen ? "visible" : "hidden"}
+        variants={variants}
+      >
+        <ul>
+          <li>About</li>
+          <li>Service</li>
+          <li>Projects</li>
+          <li className="navbar--important-button">Schedule a Call</li>
+        </ul>
+      </motion.div>
     </nav>
   );
 };
